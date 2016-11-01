1e3248238c39daf456d93a73cfc78f172645ea4f
Thibault Friedrich <tf@elqui.fr>
2016-11-01 19:13:33 +0100

    update setup command

--- setup/setup.md
+++ setup/setup.md
@@ -8,8 +8,7 @@ Once the linux accessible, we need to setup the software layer:
 $ sudo apt-get install git
 $ git clone https://github.com/home-os/home-os.git ~/home-os
 
-$ cd ~/jarvis/setup
-$ sudo chmod 755 setup.sh
-$ sudo ./setup.sh
+$ cd ~/jarvis
+$ sudo sh ./setup/setup.sh
 
 ```
--- setup/setup.sh
+++ setup/setup.sh
@@ -7,6 +7,7 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org mpg123
-
+git config --global user.name "jarvis"
+git config --global user.email "pi@jarvis"
 sudo npm install pm2 -g
 sudo npm install gulp -g
