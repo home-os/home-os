9870721b3a79d7c709845e990450b309b0ab19fe
Thibault Friedrich <tf@elqui.fr>
2016-11-02 23:25:28 +0100

    use play

--- src/os/app.js
+++ src/os/app.js
@@ -14,13 +14,14 @@ dotenv.config();
 
 // console.log(os.hostname());
 
-// const play = require('./module/music');
 
 // const Sequelize = require('sequelize');
 const packageJson = require('./../../package.json');
 const config = packageJson.config;
 const version = packageJson.version;
 const commands = require('./commands');
+const play = require('./module/music');
+
 
 var folders = {};
 
