3818ff40a624e5c12969582b6fb206c7101c36b5
Thibault Friedrich <tf@elqui.fr>
2016-11-06 13:19:09 +0100

    use regex

--- src/os/commands.js
+++ src/os/commands.js
@@ -15,15 +15,11 @@ function processCommand (stdin) {
     } else if (stdin == 'stop music') {
         cmd.id = 'stop-music';
         cmd.stdout = 'stop-music';
-    } else if (stdin.startsWith('set volume')) {
-        cmd.id = 'set-music';
-
+    } else if (stdin.match(/set volume ([0-9]+)/)) {
         var match = stdin.match(/set volume ([0-9]+)/);
-        if (match) {
-            console.log(match);
-            cmd.args.value = match[1];
-            cmd.stdout = 'set-music '+match[1];
-        }
+        cmd.id = 'set-volume';
+        cmd.args.value = match[1];
+        cmd.stdout = 'set-music '+match[1];
 
     } else {
         cmd.stdout = 'unknown command';
