2352e40321a00c93fe9e2216db3b46c6daf18fe1
Thibault Friedrich <tf@elqui.fr>
2016-11-06 20:41:11 +0100

    add file wifi

---
+++ src/os/modules/wifi.js
@@ -0,0 +1,15 @@
+const wifi = require('node-wifi');
+const networks = require('../../../data/networks/networks.json');
+
+wifi.init({
+    debug : true,
+    iface : 'wlan0'
+});
+
+function connect(callback) {
+    wifi.connect({ ssid : "Livebox-C616", password : networks["Livebox-C616"] }, callback);
+}
+
+module.exports {
+    connect: connect
+};
