631b9a837dc99891dfbed19c1c0d5e5fe439a3bb
Thibault Friedrich <tf@elqui.fr>
2016-12-06 23:09:18 +0100

    run

--- src/os/ai.js
+++ src/os/ai.js
@@ -64,7 +64,7 @@ function Ai (name) {
                 words.splice(0, 1);
                 this.say(origin, 'Ok I will do that');
                 console.log( words.join(' '), '/',  schedule.join(' '));
-                // this.run(origin, { id:'reminder', args: {  todo: words.join(' ') } }, schedule.join(' '));
+                this.run(origin, { id:'reminder', args: {  todo: words.join(' ') } }, schedule.join(' '));
             } else {
                 this.say(origin, 'Sorry but I think your request is incorrect. The proper way is "reminds me <when> to <what>."', ':thinking_face:');
             }
