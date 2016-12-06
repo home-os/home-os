d296d0ccb8f3180e5419b17d47deed5a0debf5c9
Thibault Friedrich <tf@elqui.fr>
2016-12-06 22:52:04 +0100

    add console.log to test reminder parser

--- src/os/ai.js
+++ src/os/ai.js
@@ -63,8 +63,8 @@ function Ai (name) {
                 var schedule = words.splice(0, indexOfTo);
                 words.splice(0, 1);
                 this.say(origin, 'Ok I will do that');
-                //console.log( words.join(' '), schedule.join(' '));
-                this.run(origin, { id:'reminder', args: {  todo: words.join(' ') } }, schedule.join(' '));
+                console.log( words.join(' '), '/',  schedule.join(' '));
+                // this.run(origin, { id:'reminder', args: {  todo: words.join(' ') } }, schedule.join(' '));
             } else {
                 this.say(origin, 'Sorry but I think your request is incorrect. The proper way is "reminds me <when> to <what>."', ':thinking_face:');
             }
